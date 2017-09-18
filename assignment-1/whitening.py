d17eb5fc3f20036e9039b52860aa796bff348a62
Simranjyot Gill <ssg441@nyu.edu>
2017-09-18 09:13:23 -0400

    solution to problem 1

--- assignment-1/whitening.py
+++ assignment-1/whitening.py
@@ -16,5 +16,27 @@ def whiten():
     # Translates the data so that it has zero mean (i.e. is centered at the origin).
     my_tensor -= torch.from_numpy(my_tensor.numpy().mean(axis=0))
 
+    # plotting zero mean data
+    plt.scatter(my_tensor[:, 0].numpy(), my_tensor[:, 1].numpy(),
+                s=10, marker='.', c='b')
+    plt.show()
+    plt.clf()
+
+    # calculating covariance
+    X = my_tensor.numpy()
+    cov = np.dot(X.T, X) / X.shape[0]
+    # U = eigenvectors, S = eigenvalues
+    U,S,V = np.linalg.svd(cov)
+
+    # decorrelate the data
+    decorrelated_data = np.dot(X, U)
+
+    # plotting the decorrelated data
+    plt.scatter(decorrelated_data[:, 0], decorrelated_data[:, 1],
+                s=10, marker='.', c='b')
+    plt.show()
+    plt.clf()
+
+
 if __name__ == '__main__':
     whiten()
