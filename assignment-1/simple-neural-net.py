1d0b3b54b8c2c6ad18589493ec9c7a7e91b62a97
Simranjyot Gill <ssg441@nyu.edu>
2017-09-18 15:54:52 -0400

    changed the derivative but still not working :(

--- assignment-1/simple-neural-net.py
+++ assignment-1/simple-neural-net.py
@@ -7,10 +7,8 @@ import torch.nn.functional as F
 dtype = torch.DoubleTensor
 
 # Create input and output data from
-x = torch.from_numpy(np.arange(-np.pi, np.pi, 0.01).reshape(-1,1))
-y = torch.cos(x)
-x = Variable(x, requires_grad=False)
-y = Variable(y, requires_grad=False)
+x = np.arange(-np.pi, np.pi, 0.01).reshape(-1,1)
+y = np.cos(x)
 
 # D_in is input dimension;
 # H is hidden dimension;
@@ -18,48 +16,57 @@ y = Variable(y, requires_grad=False)
 D_in, H, D_out = 1, 10, 1
 
 # Create plot for original function
-plt.scatter(x.data.numpy(), y.data.numpy(), s=5, marker='.', c='blue')
+plt.scatter(x, y, s=5, marker='.', c='blue')
 
 # Randomly initialize weights
-w1 = Variable(torch.randn(D_in, H).type(dtype), requires_grad=True)
-w2 = Variable(torch.randn(H, D_out).type(dtype), requires_grad=True)
+w1 = np.random.randn(D_in, H)
+w2 = np.random.randn(H, D_out)
 
 # Create plot for predicted function before training
-h = x.mm(w1)
-h_act = F.tanh(h)
-y_pred = h_act.mm(w2)
-plt.scatter(x.data.numpy(), y_pred.data.numpy(), s=5, marker='.', c='green')
+h = x.dot(w1)
+h_relu = np.tanh(h)
+y_pred = h_relu.dot(w2)
+# plt.scatter(x, y_pred, s=5, marker='.', c='green')
 
 # learning weights
 learning_rate = 1e-6
+learning_rate = 1e-6
 for t in range(500):
     # Forward pass: compute predicted y
-    h = x.mm(w1)
-    h_act = F.tanh(h)
-    y_pred = h_act.mm(w2)
+    h = x.dot(w1)
+    h_relu = np.tanh(h)
+    y_pred = h_relu.dot(w2)
 
-    # Compute loss
-    loss = (y_pred - y).pow(2).sum()
+    # Compute and print loss
+    loss = np.square(y_pred - y).sum()
 
-    # Use autograd to compute the backward pass. This call will compute the
-    # gradient of loss with respect to all Variables with requires_grad=True.
-    # After this call w1.grad and w2.grad will be Variables holding the gradient
-    # of the loss with respect to w1 and w2 respectively.
-    loss.backward()
+    # Backprop to compute gradients of w1 and w2 with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.T.dot(grad_y_pred)
+    calc = x * grad_y_pred
+    temp = 1 - np.tanh(x.dot(w1))**2
+    temp = temp * w2.T
+    temp = temp * calc
+    grad_w1 = temp.sum(axis=0).reshape(1,10)
+    # print(temp.shape)
+    # print(calc.shape)
+    # print(w2.shape)
+    # input()
+    # temp = temp.dot(w2) * x
+    # grad_w1 = temp * grad_y_pred
+    # print(grad_w1.shape)
+    # input()
+    # grad_h_relu = grad_y_pred.dot(w2.T)
+    # grad_h = grad_h_relu.copy()
+    # grad_w1 = x.T.dot(grad_h)
 
-    # Update weights using gradient descent; w1.data and w2.data are Tensors,
-    # w1.grad and w2.grad are Variables and w1.grad.data and w2.grad.data are
-    # Tensors.
-    w1.data -= learning_rate * w1.grad.data
-    w2.data -= learning_rate * w2.grad.data
-
-    # Manually zero the gradients after updating weights
-    w1.grad.data.zero_()
-    w2.grad.data.zero_()
+    # Update weights
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
 
 # Create plot for predicted function after training
-h = x.mm(w1)
-h_act = F.tanh(h)
-y_pred = h_act.mm(w2)
-plt.scatter(x.data.numpy(), y_pred.data.numpy(), s=5, marker='.', c='red')
+h = x.dot(w1)
+h_relu = np.tanh(h)
+y_pred = h_relu.dot(w2)
+plt.scatter(x, y_pred, s=5, marker='.', c='red')
 plt.show()
