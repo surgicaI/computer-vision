697df33460087cf009c2316a363451b5fe61fcb8
Simranjyot Gill <ssg441@nyu.edu>
2017-09-18 14:50:47 -0400

    saving code

--- assignment-1/simple-neural-net.py
+++ assignment-1/simple-neural-net.py
@@ -2,56 +2,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 from torch.autograd import Variable
+import torch.nn.functional as F
 
 dtype = torch.DoubleTensor
 
 # Create input and output data from
 x = torch.from_numpy(np.arange(-np.pi, np.pi, 0.01).reshape(-1,1))
-y = np.cos(x)
+y = torch.cos(x)
+x = Variable(x, requires_grad=False)
+y = Variable(y, requires_grad=False)
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = x.shape[0], 1, 10, 1
+# D_in is input dimension;
+# H is hidden dimension;
+# D_out is output dimension.
+D_in, H, D_out = 1, 10, 1
 
 # Create plot for original function
-plt.scatter(x.numpy(), y.numpy(), s=5, marker='.', c='blue')
+plt.scatter(x.data.numpy(), y.data.numpy(), s=5, marker='.', c='blue')
 
 # Randomly initialize weights
-w1 = torch.randn(D_in, H).type(dtype)
-w2 = torch.randn(H, D_out).type(dtype)
+w1 = Variable(torch.randn(D_in, H).type(dtype), requires_grad=True)
+w2 = Variable(torch.randn(H, D_out).type(dtype), requires_grad=True)
 
 # Create plot for predicted function before training
 h = x.mm(w1)
-h_act = np.tanh(h)
+h_act = F.tanh(h)
 y_pred = h_act.mm(w2)
-plt.scatter(x.numpy(), y_pred.numpy(), s=5, marker='.', c='green')
+plt.scatter(x.data.numpy(), y_pred.data.numpy(), s=5, marker='.', c='green')
 
 # learning weights
 learning_rate = 1e-6
-for t in range(10000):
+for t in range(500):
     # Forward pass: compute predicted y
     h = x.mm(w1)
-    h_act = np.tanh(h)
+    h_act = F.tanh(h)
     y_pred = h_act.mm(w2)
 
     # Compute loss
     loss = (y_pred - y).pow(2).sum()
 
-    # Backprop to compute gradients of w1 and w2 with respect to loss
-    grad_y_pred = 2.0 * (y_pred - y)
-    grad_w2 = h_act.t().mm(grad_y_pred)
-    grad_h_act = grad_y_pred.mm(w2.t())
-    grad_h = grad_h_act.clone()
-    grad_h[h < 0] = 0
-    grad_w1 = x.t().mm(grad_h)
+    # Use autograd to compute the backward pass. This call will compute the
+    # gradient of loss with respect to all Variables with requires_grad=True.
+    # After this call w1.grad and w2.grad will be Variables holding the gradient
+    # of the loss with respect to w1 and w2 respectively.
+    loss.backward()
 
-    # Update weights using gradient descent
-    w1 -= learning_rate * grad_w1
-    w2 -= learning_rate * grad_w2
+    # Update weights using gradient descent; w1.data and w2.data are Tensors,
+    # w1.grad and w2.grad are Variables and w1.grad.data and w2.grad.data are
+    # Tensors.
+    w1.data -= learning_rate * w1.grad.data
+    w2.data -= learning_rate * w2.grad.data
+
+    # Manually zero the gradients after updating weights
+    w1.grad.data.zero_()
+    w2.grad.data.zero_()
 
 # Create plot for predicted function after training
 h = x.mm(w1)
-h_act = np.tanh(h)
+h_act = F.tanh(h)
 y_pred = h_act.mm(w2)
-plt.scatter(x.numpy(), y_pred.numpy(), s=5, marker='.', c='red')
+plt.scatter(x.data.numpy(), y_pred.data.numpy(), s=5, marker='.', c='red')
 plt.show()
