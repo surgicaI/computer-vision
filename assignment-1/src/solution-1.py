98c442eddb2d74c144217272acd19bab5980f1f4
Simranjyot Gill <ssg441@nyu.edu>
2017-10-12 17:33:03 -0400

    adds comments to solution 1 and solution 4

--- assignment-1/src/solution-1.py
+++ assignment-1/src/solution-1.py
@@ -5,14 +5,18 @@ def convolve(image):
     # kernel = [1 2 1]/4
     rows, cols = image.shape
     # assuming valid boundary conditions
+    # convolution of [1 2 1] / 4 kernel with the image
     row_convolution = 2*image[:, 1:cols-1]
     row_convolution = row_convolution + 1*image[:, :cols-2] + 1*image[:, 2:]
     image = 0.25 * row_convolution
 
+    # the output of row convolution is convoluted again
+    # with ([1 2 1]/4)transpose
     rows, cols = image.shape
     col_convolution = 2*image[1:rows-1, :]
     col_convolution = col_convolution + 1*image[:rows-2, :] + 1*image[2:, :]
     image = 0.25 * col_convolution
+    # final size of the image is [image.rows - 2, image_cols -2]
     return image
 
 
@@ -25,14 +29,20 @@ if __name__ == '__main__':
         quit()
 
     kernel_width = 2
-    while kernel_width % 2 == 0 or kernel_width <= 0:
-        kernel_width = int(input('please input kernel width(odd natural number):'))
+    while kernel_width % 2 == 0 or kernel_width < 3:
+        kernel_width = int(input('please input kernel width(odd natural number >= 3):'))
 
-    # - So can smooth with small-width kernel, repeat, and get
-    # same result as larger-width kernel would have.
-    # - Convolving two times with Gaussian kernel of width σ is
-    # same as convolving once with kernel of width σ√2
-    num_iterations = 10
+    # Applying multiple, successive Gaussian blurs to an image has the same effect as applying a single, larger Gaussian blur, whose radius is the square root of the sum of the squares of the blur radii that were actually applied.
+    # For example, applying successive Gaussian blurs with radii of 6 and 8 gives the same results as applying a single Gaussian blur of radius 10, since  (6^2 + 8^2)^0.5 = 10.
+    # initial radius of gausian kernel = sigma
+    # after 2 iterations radius = (1^2 + 1^2)^0.5 signma = 2^0.5 sigma
+    # after 3 iterations, radius = (2^(0.5*2) + 1^2) sigma = 3^0.5 sigma
+    # after n iterations, radius = n^0.5 sigma
+    # kernel_width = 2*radius + 1
+    # in our case sigma = 1
+    num_iterations = ((kernel_width - 1) / 2)**2
+    num_iterations = int(num_iterations)
+    print('num_iterations:', num_iterations)
     for _ in range(num_iterations):
         image = convolve(image)
 
--- assignment-1/src/solution-4.py
+++ assignment-1/src/solution-4.py
@@ -4,43 +4,57 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 sfm = sio.loadmat('res/sfm_points.mat')
+LOGS_ENABLED = False
 
 image_points = sfm['image_points']
 
-#Calculate centroid and subtract from image points
+# Compute the translations t
+# i directly by computing the centroid of point
+# in each image i.
+# Center the points in each image by subtracting off the centroid, so that
+# the points have zero mean
 for i in range(0,10):
     x_centroid=sum(image_points[0,:,i])/len(image_points[0,:,i])
     y_centroid=sum(image_points[1,:,i])/len(image_points[1,:,i])
     image_points[0,:,i] = image_points[0,:,i] - x_centroid
     image_points[1,:,i] = image_points[1,:,i] - y_centroid
 
-    #For first camera
+    # The script should print out the Mi and ti
+    # for the first camera
     if i==0:
         print("T for first camera")
-        print(x_centroid)
-        print(y_centroid)
+        print('x-centroid', x_centroid)
+        print('y-centroid', y_centroid)
 
 
-#Creating 2mxn Matrix
+# Construct the 2m by n measurement matrix W from the centered data.
 w = image_points[:,:,0]
 for i in range(1,10):
     w=np.vstack((w,image_points[:,:,i]))
 
-
-#SVD of W
+# Perform an SVD decomposition of W into UDV T
 u,s,v=np.linalg.svd(w)
 
-#M for first camera
+# The camera locations Mi
+# can be obtained from the first three columns
+# of U multiplied by D(1 : 3, 1 : 3), the first three singular values
 M=np.dot(u[:,0:3],np.array([[s[0],0,0],[0,s[1],0],[0,0,s[2]]]))
+# The script shouldprint out the Mi and ti
+# for the first camera
 print("M for first camera")
 print(M[0:2])
 
 
-#Printing 10 coordinates
+# The script should print out the Mi and ti
+# for the first camera and also the 3D coordinates of
+# the first 10 world points.
 print("First 10 coordinates")
 print(np.transpose(v[0:3])[0:10])
 
-#Plotting coordinates
+# You can verify your answer by plotting the 3D world points out. using
+# the plot3 command. The rotate3d command will let you rotate the
+# plot. This functionality is replicated in Python within the matplotlib
+# package
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 for i in range(0,600):
